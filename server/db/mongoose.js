2a45ae643b3e3467eabde8c409aecc1f2598a300
Anuradha118 <anuradha.sahoo1993@gmail.com>
2017-12-23 20:09:26 +0530

    Added Heroku connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
 // console.log(process.env.MONGO)
-// mongoose.connect('mongodb://anuradha:todo@ds163836.mlab.com:63836/todoapp');
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://anuradha:todo@ds163836.mlab.com:63836/todoapp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports={mongoose};
