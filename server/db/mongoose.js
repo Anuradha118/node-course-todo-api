9e09989feb326308fd1c45fdec6720527425d01c
Anuradha118 <anuradha.sahoo1993@gmail.com>
2017-12-24 12:20:06 +0530

    Add POST/users route and token method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,8 @@ var mongoose=require('mongoose');
 
 
 mongoose.Promise=global.Promise;
-// console.log(process.env.MONGO)
-mongoose.connect('mongodb://anuradha:todo@ds163836.mlab.com:63836/todoapp');
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
+// console.log('Monggose File',process.env.MONGODB_URI);
+// mongoose.connect('mongodb://anuradha:todo@ds163836.mlab.com:63836/todoapp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports={mongoose};
