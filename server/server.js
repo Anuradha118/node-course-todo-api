94e5916b636d357a4abadbd5033f8406b36e04db
Anuradha118 <anuradha.sahoo1993@gmail.com>
2017-12-31 00:07:59 +0530

    Updated the delete function name

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ app.delete('/todos/:id',authenticate,(req,res)=>{
     if(!ObjectID.isValid(id)){
         return res.status(404).send();
     }
-    Todo.findByOneAndRemove({
+    Todo.findOneAndRemove({
         _id:id,
         _creator:req.user._id
     }).then((todo)=>{
