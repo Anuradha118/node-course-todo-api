0a93780a7b4b0f745ebedad6d8462b086e16b5b5
Anuradha118 <anuradha.sahoo1993@gmail.com>
2017-12-30 19:06:40 +0530

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
-if(env==='development'||env==='test'){
-    process.env.PORT=3000;
-    process.env.MONGODB_URI='mongodb://localhost:27017/TodoApp';
-}else if(env === 'production'){
-    process.env.MONGODB_URI='mongodb://anuradha:todo@ds163836.mlab.com:63836/todoapp';
-}
 
-// module.exports={configs};
+if(env==='development'||env==='test'){
+    var config=require('./config.json');
+    var envConfig=config[env];
+
+    Object.keys(envConfig).forEach((key)=>{
+        process.env[key]=envConfig[key];
+    });
+}
